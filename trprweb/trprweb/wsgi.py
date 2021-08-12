677a907981a7994bf6154bc65795288a6854d5d7
seshasrinath <seshasreenath@gmail.com>
2021-08-12 16:27:38 +0530

    Update wsgi.py

--- trprweb/trprweb/wsgi.py
+++ trprweb/trprweb/wsgi.py
@@ -11,6 +11,6 @@ import os
 import trprweb
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trprweb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 application = get_wsgi_application()
